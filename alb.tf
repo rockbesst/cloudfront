1e88a2c07f71147c8b1f826bdd58428ca441a2fc
Rockbesst Win <rockbesst@gmail.com>
2021-07-02 09:05:00 +0300

    added Web2 for ALB

--- alb.tf
+++ alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "MainLoadBalancer" {
  	name = "MainLoadBalancer"
  	load_balancer_type = "application"
  	security_groups = [data.aws_security_group.mainSecGroup.id]
- 	subnets = [data.aws_subnet.sub1.id]
+ 	subnets = [data.aws_subnet.sub1.id, data.aws_subnet.sub2.id]
 }
 
  resource "aws_lb_listener" "listener_http" {
@@ -25,7 +25,15 @@ resource "aws_lb" "MainLoadBalancer" {
   	target_id        = aws_instance.WebServer1.id
    	port             = 80
  }
+ resource "aws_lb_target_group_attachment" "attach2_to_tg_main" {
+ 	target_group_arn = aws_lb_target_group.tg_main.arn
+  	target_id        = aws_instance.WebServer2.id
+   	port             = 80
+ }
  # Subnets
  data "aws_subnet" "sub1"{
 	 id = "subnet-0c95ec66"
+ }
+ data "aws_subnet" "sub2"{
+	 id = "subnet-ad3c88d1"
  }
--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,22 @@
 resource "aws_instance" "WebServer1" {
 	ami = data.aws_ami.amazon_linux.id
 	instance_type = var.instance_type
-	availability_zone = "eu-central-1b"
+	availability_zone = "eu-central-1a"
 	vpc_security_group_ids = [data.aws_security_group.mainSecGroup.id]
 	key_name = var.ssh_key
 	associate_public_ip_address = var.allow_public_ip
 	tags = merge(var.tags, {Name = "WebServer1"})
 }
 
+resource "aws_instance" "WebServer2" {
+	ami = data.aws_ami.amazon_linux.id
+	instance_type = var.instance_type
+	availability_zone = "eu-central-1b"
+	vpc_security_group_ids = [data.aws_security_group.mainSecGroup.id]
+	key_name = var.ssh_key
+	associate_public_ip_address = var.allow_public_ip
+	tags = merge(var.tags, {Name = "WebServer2"})
+}
 # AMI Search
 data "aws_ami" "amazon_linux" {
 	owners = ["amazon"]
